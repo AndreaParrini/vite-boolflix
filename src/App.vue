73982caa71f94d3306f9c6dc4166a27b10f1c14c
AndreaParrini <andrea.parrini26@gmail.com>
2024-02-19 19:15:54 +0100

    add ajax call to serie tv and push in array
    add function to convert vote

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,14 @@ export default {
     return {
       searchText: '',
       movies: [],
-      base_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=1624b90122a6e598e03c4e5d2ad8bd21'
+      base_api_url_movies: 'https://api.themoviedb.org/3/search/movie?api_key=1624b90122a6e598e03c4e5d2ad8bd21&language=it-IT',
+      base_api_url_serietv: 'https://api.themoviedb.org/3/search/tv?api_key=1624b90122a6e598e03c4e5d2ad8bd21&language=it-IT'
+
     }
   },
   methods: {
     searchMovie() {
-      const url = `${this.base_api_url}&query=${this.searchText}`
+      const url = `${this.base_api_url_movies}&query=${this.searchText}`
       console.log(url);
       axios
         .get(url)
@@ -21,11 +23,33 @@ export default {
           console.log(response.data.results);
           this.movies = response.data.results;
         })
-
+    },
+    searchSerieTv() {
+      const url = `${this.base_api_url_serietv}&query=${this.searchText}`
+      console.log(url);
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data.results);
+          const allSerieTV = response.data.results;
+          for (let i = 0; i < allSerieTV.length; i++) {
+            const element = allSerieTV[i];
+            this.movies.push(element)
+          }
+        })
+    },
+    searchAll() {
+      this.searchMovie();
+      this.searchSerieTv();
     },
     urlImage(codiceIso) {
       const url = `/images/icons8-${codiceIso}-100.png`
       return url;
+    },
+    conversionVote(vote) {
+      const newVote = (vote * 5) / 10;
+      return Math.floor(newVote);
     }
   }
 }
@@ -33,25 +57,22 @@ export default {
 
 <template>
   <input type="text" name="searchMovie" id="searchMovie" v-model="searchText">
-  <button @click="searchMovie">Search</button>
+  <button @click="searchAll">Search</button>
   <div v-for="movie in  movies " style="border: 2px solid black;">
     <ul>
       <li>
-        <div>{{ movie.title }}</div>
+        <div>{{ movie.title ? movie.title : movie.name }}</div>
       </li>
       <li>
-        <div>{{ movie.original_title }}</div>
+        <div>{{ movie.original_title ? movie.original_title : movie.original_name }}</div>
       </li>
       <li>
-        <!-- <div>{{ movie.original_language }}</div> -->
         <div>
-          <img v-if="urlImage(movie.original_language)" :src="urlImage(movie.original_language)"
-            :alt="movie.original_language">
-          <div v-else>pipppo</div>
+          <img :src="urlImage(movie.original_language)" :alt="movie.original_language">
         </div>
       </li>
       <li>
-        <div>{{ movie.vote_average }}</div>
+        <div>{{ conversionVote(movie.vote_average) }}</div>
       </li>
     </ul>
 
