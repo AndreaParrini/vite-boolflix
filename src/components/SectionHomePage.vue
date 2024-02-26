869dd47a0a0862507201d7b107a9c3096cc5bb13
AndreaParrini <andrea.parrini26@gmail.com>
2024-02-26 23:09:04 +0100

    add slider of trend movie in home page
    add in style.css class col-2
    
    create a new component and import in section home page
    
    modify style of div intro

--- src/components/SectionHomePage.vue
+++ src/components/SectionHomePage.vue
@@ -1,8 +1,12 @@
 <script>
-import { store } from '../store';
+import { store } from '../store.js';
+import SliderItem from './SliderItem.vue'
 
 export default {
     name: 'SectionHomePage',
+    components: {
+        SliderItem
+    },
     data() {
         return {
             store
@@ -13,20 +17,42 @@ export default {
 
 <template>
     <section class="home_page">
-        <div>
-            Benvenuto in Boolflix, il sito per la ricerca e la scoperta di nuovi film\serie tv da vedere.
-            Nel nostro sito potrai avere un grande varietà di scelta tra più di migliaia di film, con recensioni da
-            parte di tutti i nostri utenti, cosi da poter avere già da subito una prima senzazione.
+        <div class="intro">
+            <h2>
+                Benvenuto in <span>Boolflix</span>
+            </h2>
+            <div>
+                Il sito per la ricerca e la scoperta di nuovi film/serie tv da vedere.
+            </div>
+            <div>
+                Nel nostro sito potrai avere un grande varietà di scelta tra più di migliaia di film, con recensioni da
+                parte di tutti i nostri utenti, cosi da poter avere già da subito una prima senzazione.
+            </div>
         </div>
+        <SliderItem></SliderItem>
     </section>
 </template>
 
 <style scoped>
 .home_page {
-    height: 30vh;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    color: white;
+
+    & .intro {
+        text-align: center;
+
+        &>h2 {
+            margin: 1rem 0;
+
+            & span {
+                color: var(--boolflix-red);
+                text-transform: uppercase;
+            }
+        }
+
+        & div {
+            margin: 0.5rem 0;
+        }
+    }
+
 }
 </style>
---
+++ src/components/SliderItem.vue
@@ -0,0 +1,114 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'SliderItem',
+    data() {
+        return {
+            activeImage: 0,
+            trendMovie: [],
+        }
+    },
+    methods: {
+        getTrendMovie() {
+            axios
+                .get('https://api.themoviedb.org/3/trending/all/week?api_key=1624b90122a6e598e03c4e5d2ad8bd21&language=it-IT')
+                .then((response) => {
+                    let loader = 0;
+                    for (let i = 0; i < response.data.results.length; i++) {
+                        const movie = response.data.results[i];
+                        if (loader < 6) {
+                            //this.trendMovie.push('https://image.tmdb.org/t/p/w500' + movie.poster_path);
+                            this.trendMovie.push(movie);
+                            loader++;
+                        }
+                    }
+                });
+        },
+        prev() {
+            this.activeImage--;
+            if (this.activeImage < 0) {
+                this.activeImage = this.trendMovie.length - 1;
+            }
+        },
+        next() {
+            this.activeImage++;
+            if (this.activeImage > this.trendMovie.length - 1) {
+                this.activeImage = 0;
+            }
+        }
+    },
+    created() {
+        this.getTrendMovie();
+        setInterval(() => {
+            this.activeImage++;
+            if (this.activeImage > this.trendMovie.length - 1) {
+                this.activeImage = 0;
+            }
+
+        }, "3000")
+    }
+}
+</script>
+
+<template>
+    <div class="popular">
+        <h1>I trend di questa settimana</h1>
+        <div class="row">
+            <div class=" col-2" v-for="(movie, index) in trendMovie">
+                <div class="thumbs" :class="index === activeImage ? 'active' : ''" @click="activeImage = index">
+                    <img width="200" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="">
+                    <div class="titolo">{{ movie.title ? movie.title : movie.name }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.popular {
+    text-align: center;
+    padding-top: 2rem;
+
+    & h1 {
+        text-transform: uppercase;
+        margin: 2rem 0;
+    }
+
+    & .slides {
+        width: 100%;
+
+        &>img {
+            width: 300px;
+            height: 500px;
+        }
+    }
+
+    & .thumbs {
+        width: 100%;
+
+        & img {
+            width: 100%;
+        }
+
+        &.active {
+            &>img {
+                filter: drop-shadow(0 1rem 0.6rem black);
+                border: 3px solid black;
+                scale: 1.1;
+            }
+
+            & .titolo {
+                display: block;
+            }
+        }
+    }
+
+    & .titolo {
+        display: none;
+        margin-top: 2rem;
+        color: white;
+        text-transform: uppercase;
+    }
+}
+</style>
